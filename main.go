380ac203312a2d628fcc22f1ef57f5ff396526ae
Massimiliano Pippi <mpippi@gmail.com>
2015-01-02 17:47:45 +0100

    added main

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/alecthomas/kingpin.v1"
+)
+
+var (
+	debug = kingpin.Flag("debug", "Enable debug mode.").Bool()
+	src   = kingpin.Arg("SRC", "Flickr SET to sync").Required().String()
+	dest  = kingpin.Arg("DEST", "Destination path").String()
+)
+
+func main() {
+	kingpin.CommandLine.Help = "A Flickr syncing tool"
+	kingpin.Version("0.0.1")
+	kingpin.Parse()
+	fmt.Printf("Would sync: %s to %s", *src, *dest)
+}
