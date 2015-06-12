9edff19c0dda4efd8b6d40020005bd4a1ae16e74
Massimiliano Pippi <mpippi@gmail.com>
2015-06-12 18:29:11 +0200

    new project layout

---
+++ command/commmand.go
@@ -0,0 +1,19 @@
+package command
+
+import (
+	"fmt"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	debug = kingpin.Flag("debug", "Enable debug mode.").Bool()
+	src   = kingpin.Arg("SRC", "Flickr SET to sync").Required().String()
+	dest  = kingpin.Arg("DEST", "Destination path").String()
+)
+
+func Main() {
+	kingpin.CommandLine.Help = "A Flickr syncing tool"
+	kingpin.Version("0.0.1")
+	kingpin.Parse()
+	fmt.Printf("Would sync: %s to %s", *src, *dest)
+}
--- main.go
+++ main.go
@@ -1,19 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"gopkg.in/alecthomas/kingpin.v2"
-)
-
-var (
-	debug = kingpin.Flag("debug", "Enable debug mode.").Bool()
-	src   = kingpin.Arg("SRC", "Flickr SET to sync").Required().String()
-	dest  = kingpin.Arg("DEST", "Destination path").String()
-)
+import "github.com/masci/flick-rsync/command"
 
 func main() {
-	kingpin.CommandLine.Help = "A Flickr syncing tool"
-	kingpin.Version("0.0.1")
-	kingpin.Parse()
-	fmt.Printf("Would sync: %s to %s", *src, *dest)
+	command.Main()
 }
