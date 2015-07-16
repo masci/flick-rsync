d66c81c90a52e54c66b6cc36c892078ea09cdf70
Massimiliano Pippi <mpippi@gmail.com>
2015-07-16 18:52:17 +0200

    added package for testing support functions

---
+++ testutils/testutils.go
@@ -0,0 +1,12 @@
+package testutils
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Expect(t *testing.T, a interface{}, b interface{}) {
+	if a != b {
+		t.Errorf("Expected %v (type %v) - Got %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
+	}
+}
---
+++ testutils/testutils_test.go
@@ -0,0 +1,13 @@
+package testutils
+
+import (
+	"testing"
+)
+
+func TestExpect(t *testing.T) {
+	t2 := *t
+	Expect(&t2, 1, 2)
+	if !t2.Failed() {
+		t.Errorf("Expect should fail")
+	}
+}
