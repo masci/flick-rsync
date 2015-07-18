757a661a7db1a87cb6d0056f57836b41844aab88
Massimiliano Pippi <mpippi@gmail.com>
2015-07-18 16:15:08 +0200

    added docs

--- command/path.go
+++ command/path.go
@@ -11,15 +11,29 @@ import (
 var re *regexp.Regexp
 
 func init() {
+	// compile the regex matching flickr paths
 	re = regexp.MustCompile(`(\w+)@flickr:(/[\w|/]*)?`)
 }
 
+// determine if a string contains a valid flickr Set Identifier
 func isSetId(set string) bool {
 	s := strings.Trim(set, "/")
 	_, err := strconv.Atoi(s)
 	return err == nil
 }
 
+// Parse component of a flickr path of the form:
+//
+// `<username>@flickr:<setId>`
+//
+// setId can be omitted, like in:
+//
+// `<username>@flickr:<setId>`
+//
+// or set to `/`:
+//
+// `<username>@flickr:/`
+//
 func ParseFilckrPath(path string) (string, string, error) {
 	match := re.FindStringSubmatch(path)
 
