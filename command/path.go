30947874f841ddc0c67990f17ea93577c11a8dda
Massimiliano Pippi <mpippi@gmail.com>
2015-07-21 19:39:49 +0200

    changed flickr path definition

--- command/path.go
+++ command/path.go
@@ -5,21 +5,13 @@ import (
 	"fmt"
 	"regexp"
 	"strconv"
-	"strings"
 )
 
 var re *regexp.Regexp
 
 func init() {
 	// compile the regex matching flickr paths
-	re = regexp.MustCompile(`(\w+)@flickr:(/[\w|/]*)?`)
-}
-
-// determine if a string contains a valid flickr Set Identifier
-func isSetId(set string) bool {
-	s := strings.Trim(set, "/")
-	_, err := strconv.Atoi(s)
-	return err == nil
+	re = regexp.MustCompile(`^(\w+)@flickr:(\d+)?$`)
 }
 
 // Parse component of a flickr path of the form:
@@ -28,24 +20,25 @@ func isSetId(set string) bool {
 //
 // setId can be omitted, like in:
 //
-// `<username>@flickr:<setId>`
+// `<username>@flickr:`
 //
-// or set to `/`:
-//
-// `<username>@flickr:/`
+// an empty setId has different meanings, depending on the context
 //
 func ParseFilckrPath(path string) (string, string, error) {
+	var isSetId = func(set string) bool {
+		_, err := strconv.Atoi(set)
+		return err == nil
+	}
+
 	match := re.FindStringSubmatch(path)
 
 	if len(match) == 3 {
 		u := match[1]
 		s := match[2]
-		if s == "" || s == "/" || isSetId(s) {
+		if s == "" || isSetId(s) {
 			return u, s, nil
 		}
 		return "", "", errors.New(fmt.Sprintf("Not a valid Flickr Set id: %s", s))
-	} else if len(match) > 1 {
-		return match[1], "", nil
 	}
 
 	return "", "", errors.New(fmt.Sprintf("Not a valid Flickr path: %s", path))
