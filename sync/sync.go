9bfdec56fa08eb682943928174ae706110c0045d
Massimiliano Pippi <mpippi@gmail.com>
2015-07-21 19:40:02 +0200

    first draft

--- sync/sync.go
+++ sync/sync.go
@@ -2,6 +2,7 @@ package sync
 
 import (
 	"fmt"
+	"path/filepath"
 
 	"github.com/masci/flickr.go/flickr"
 )
@@ -13,6 +14,16 @@ func SyncRemote(client *flickr.FlickrClient, setId string, destPath string) {
 		destPath = "."
 	}
 
-	fmt.Println("Sync flickr set", setId, "with local folder", destPath)
+	// normalize destination path
+	if !filepath.IsAbs(destPath) {
+		destPath, _ = filepath.Abs(destPath)
+	}
 
+	// if setId is empty, assume "all sets" need to be synced
+	if setId == "" {
+		// retrieve all sets
+		fmt.Println("Syncing al flickr sets with local folder", destPath)
+	} else {
+		fmt.Println("Sync flickr set", setId, "with local folder", destPath)
+	}
 }
